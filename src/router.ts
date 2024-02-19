133ebd835e39deec023c353624a3a0269ab03a55
Adnan <59744727+AdnanJoraid@users.noreply.github.com>
2024-02-19 18:17:11 -0500

    Add Error handler

--- src/router.ts
+++ src/router.ts
@@ -56,21 +56,8 @@ router.post(
 );
 router.delete("/update/:id", deleteUpdate);
 
-router.get("/updatepoint", () => {});
-router.get("/updatepoint/:id", () => {});
-router.put(
-  "/updatepoint/:id",
-  body("name").optional().isString(),
-  body("description").optional().isString(),
-  () => {},
-);
-router.post(
-  "/updatepoint",
-  body("name").isString(),
-  body("description").isString(),
-  body("updateId").exists().isString(),
-  () => {},
-);
-router.delete("/updatepoint/:id", () => {});
-
+router.use((err, req, res, next) => {
+  console.log(err);
+  res.json({ message: "in router handler" });
+});
 export default router;

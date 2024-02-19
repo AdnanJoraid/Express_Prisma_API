d75c84ad72dcbbe6e1fa13a220e5f07ff36f8963
Adnan <59744727+AdnanJoraid@users.noreply.github.com>
2024-02-19 17:47:46 -0500

    Finish handler for update endpoints

--- src/router.ts
+++ src/router.ts
@@ -1,12 +1,26 @@
 import { Router } from "express";
 import { body, oneOf, validationResult } from "express-validator";
 import { handleInputErrors } from "./modules/middleware";
+import {
+  createProduct,
+  deleteProduct,
+  getOneProduct,
+  getProducts,
+} from "./handlers/product";
+import {
+  createUpdate,
+  deleteUpdate,
+  getOneUpdate,
+  getUpdates,
+  updateUpdate,
+} from "./handlers/update";
 const router = Router();
 
-router.get("/product", (req, res) => {
-  res.json({ message: "hello from product" });
-});
-router.get("/product/:id", () => {});
+/*
+ Product
+*/
+router.get("/product", getProducts);
+router.get("/product/:id", getOneProduct);
 router.put(
   "/product/:id",
   body("name").isString(),
@@ -17,27 +31,30 @@ router.post(
   "/product",
   body("name").isString(),
   handleInputErrors,
-  (req, res) => {},
+  createProduct,
 );
-router.delete("/product/:id", () => {});
-
-router.get("/update", () => {});
-router.get("/update/:id", () => {});
+router.delete("/product/:id", deleteProduct);
+/*
+ Update
+*/
+router.get("/update", getUpdates);
+router.get("/update/:id", getOneUpdate);
 router.put(
   "/update/:id",
   body("title").optional(),
   body("body").optional(),
-  body("status").isIn(["IN_PROGRESS", "SHIPPED", "DEPRECATED"]),
+  body("status").isIn(["IN_PROGRESS", "SHIPPED", "DEPRECATED"]).optional(),
   body("version").optional(),
-  () => {},
+  updateUpdate,
 );
 router.post(
   "/update",
   body("title").exists().isString(),
   body("body").exists().isString(),
-  () => {},
+  body("productId").exists().isString(),
+  createUpdate,
 );
-router.delete("/update/:id", () => {});
+router.delete("/update/:id", deleteUpdate);
 
 router.get("/updatepoint", () => {});
 router.get("/updatepoint/:id", () => {});
